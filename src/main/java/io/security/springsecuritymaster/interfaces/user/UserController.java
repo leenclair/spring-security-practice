d026ff8634b8a153aadfa2ab03f0d0625e89cfc2
leenclair <leesu9317@gmail.com>
2025-05-11 20:20:53 +0900

    custom logout

--- src/main/java/io/security/springsecuritymaster/interfaces/user/UserController.java
+++ src/main/java/io/security/springsecuritymaster/interfaces/user/UserController.java
@@ -3,8 +3,13 @@ package io.security.springsecuritymaster.interfaces.user;
 import io.security.springsecuritymaster.domain.user.UserCommand;
 import io.security.springsecuritymaster.domain.user.UserInfo;
 import io.security.springsecuritymaster.domain.user.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -34,5 +39,15 @@ public class UserController {
         log.info("signup response: {}", response);
 
         return "redirect:/";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        Authentication authentication = SecurityContextHolder.getContextHolderStrategy().getContext().getAuthentication();
+        if(authentication != null) {
+            new SecurityContextLogoutHandler().logout(request, response, authentication);
+        }
+
+        return "redirect:/login";
     }
 }
